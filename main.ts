243fcfbd9884fd680e4c031c226f91f10f07b38d
Lúcia Abreu <26051893+luciabreu@users.noreply.github.com>
2020-11-22 03:24:11 +0000

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,28 +1,41 @@
 NFC.nfcEvent(function () {
-    if (NFC.getUID() == cardID1) {
-        basic.showIcon(IconNames.Heart)
+    if (NFC.getUID() == Atlantic) {
         radio.sendNumber(1)
-    } else if (NFC.getUID() == cardID2) {
-        basic.showIcon(IconNames.Yes)
+    } else if (NFC.getUID() == Indic) {
         radio.sendNumber(2)
-    } else if (NFC.getUID() == cardID3) {
-        basic.showIcon(IconNames.House)
+    } else if (NFC.getUID() == Pacific) {
         radio.sendNumber(3)
-    } else if (NFC.getUID() == cardID4) {
+    } else if (NFC.getUID() == Antartic) {
         radio.sendNumber(4)
-    } else if (NFC.getUID() == cellID5) {
+    } else if (NFC.getUID() == Artic) {
         radio.sendNumber(5)
+    } else if (NFC.getUID() == Intro_1) {
+        radio.sendNumber(6)
+    } else if (NFC.getUID() == Intro_2) {
+        radio.sendNumber(7)
+    } else if (NFC.getUID() == Intro_3) {
+        radio.sendNumber(8)
+    } else if (NFC.getUID() == turtle) {
+        radio.sendNumber(9)
     }
 })
-let cellID5 = ""
-let cardID4 = ""
-let cardID3 = ""
-let cardID2 = ""
-let cardID1 = ""
+let Intro_3 = ""
+let Intro_2 = ""
+let Intro_1 = ""
+let Artic = ""
+let turtle = ""
+let Pacific = ""
+let Antartic = ""
+let Indic = ""
+let Atlantic = ""
 NFC.NFC_setSerial(SerialPin.P14, SerialPin.P13)
-cardID1 = "76456E95"
-cardID2 = "76C75095"
-cardID3 = "DDFAD604"
-cardID4 = "2D08D404"
-cellID5 = "59F57EA2"
-radio.setGroup(20)
+Atlantic = "76456E95"
+Indic = "76C75095"
+Antartic = "DDFAD604"
+Pacific = "2D08D404"
+turtle = "59F57EA2"
+Artic = "6EE5FBA9"
+Intro_1 = "8EE3DC79"
+Intro_2 = "A4F6DC79"
+Intro_3 = "B1DADC79"
+radio.setGroup(31)
